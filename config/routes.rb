a0c3aae49d0097f9264d048917be359e832ce29e
Jim <jympire@gmail.com>
2018-06-16 11:12:48 -0700

    create user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,6 @@ Rails.application.routes.draw do
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'edit', sign_up: 'register'}
+  
+  resources :users, only: [:show]
 end
